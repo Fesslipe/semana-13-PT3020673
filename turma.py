bd8105da83b9af188adca14bfe1fb7a72c31401b
Fesslipe <119904645+Fesslipe@users.noreply.github.com>
2025-01-16 12:20:48 -0300

    Update turma.py

--- turma.py
+++ turma.py
@@ -1,4 +1,4 @@
-class TurmaClass:
+class Turma:
   def __init__(self):
     self.turma = [];
     self.menorNota = None;
@@ -6,12 +6,13 @@ class TurmaClass:
 
   def cadastrarAlunos(self, alunos):    
     for i in alunos:      
-      if(i.nota <= 11 and i.nota >= 0):
+      if(i.nota <= 10 and i.nota >= 0):
         self.turma.append(i); 
         if((self.menorNota == None) or (self.menorNota.nota > i.nota)):
           self.menorNota = i;
         if((self.maiorNota == None) or (self.maiorNota.nota < i.nota)):
-          self.maiorNota = i;            
+          self.maiorNota = i;     
+            
 
   def mostrarAlunos(self):  
     print('Quantidade de alunos:', len(self.turma));
